9d66df7b8e23d0fe7abfbf3c5ac265579a71ee00
JoeKrieger02 <krieger.joachim@hotmail.be>
2018-07-23 19:46:40 +0200

    updateShelf function

--- src/App.js
+++ src/App.js
@@ -17,48 +17,36 @@ class App extends React.Component {
 componentDidMount(){
 	BooksAPI.getAll().then((books) => {
 		this.setState({ books })
+		console.log(JSON.stringify(books))
 	})
 }
 
 
-
-updatedBook=(event, books)=> {
-	this.setState({books: event.target.value})
-  
-}
-
-
-/*
-getShelf= (event, book) => {
-  	const shelf = event.target.value;
-  	event.preventDefault()
-  	book.shelf = event.target.value
-  	this.setState({ book })
-	
-	BooksAPI.update(book, shelf).then (updated => {
-    	this.setState(state => ({
-        book: state.book
-          .filter(b => b.id !== book.id)
-          .concat([book])
-      }))
+updateShelf=(book, shelf)=> {
+	BooksAPI.update(book,shelf).then(()=>{
+      //this.getBooks()
+      BooksAPI.getAll().then((books)=>{
+      this.setState({ books })
     })
+    })
+
   }
 
-*/
+
   render() {
     return (
     <BrowserRouter>
       <div className="app">
       	<Route exact path="/" render={()=> (
     		<BookPage 
-    			UpdatedBook={this.UpdatedBook}
+    			updateShelf={this.updateShelf}
 				books={this.state.books}
 			/>
 		)}/>
 		<Route path="/SearchPage" render={()=> (
       		<SearchPage
              	books={this.state.books}
-				UpdatedBook={this.UpdatedBook}
+				updateShelf={this.updateShelf}
 			/>
 		)}/>
 	</div> 
