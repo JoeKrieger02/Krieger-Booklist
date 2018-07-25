7478512ebb0cd3c779c543cd0e7c06022af36889
JoeKrieger02 <krieger.joachim@hotmail.be>
2018-07-25 17:32:23 +0200

    cleaner

--- src/BookPage.js
+++ src/BookPage.js
@@ -1,23 +1,21 @@
 import React from "react"
-//import * as BooksAPI from './BooksAPI'
 import "./App.css"
 import { Link } from "react-router-dom"
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types"
 
 class BookPage extends React.Component {
-  static propTypes={
-  updateShelf:PropTypes.func.isRequired
+  static propTypes = {
+    updateShelf: PropTypes.func.isRequired
   }
-  
-  render() { 
+
+  render() {
     this.props.books.map((book) => {
-      console.log(book)
-      return book 
+      return book
     })
     return (
       <div className="list-books">
         <div className="list-books-title">
-          <h1>MyReads</h1>
+          <h1>KriegerReads</h1>
         </div>
         <div className="list-books-content">
           <div>
@@ -25,44 +23,52 @@ class BookPage extends React.Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {this.props.books
-                      .filter((book) => book.shelf === "currentlyReading")
-                      .map((book, index) => {
-                        return (
-                          <li key={index}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div
-                                  className="book-cover"
-                                  style={{
-                                    width: 128,
-                                    height: 193,
-                                    backgroundImage:
-                                      'url("' + book.imageLinks.thumbnail + '")'
+                  {this.props.books
+                    .filter((book) => book.shelf === "currentlyReading")
+                    .map((book, index) => {
+                      return (
+                        <li key={index}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div
+                                className="book-cover"
+                                style={{
+                                  width: 128,
+                                  height: 193,
+                                  backgroundImage:
+                                    'url("' + book.imageLinks.thumbnail + '")'
+                                }}
+                              />
+                              <div className="book-shelf-changer">
+                                <select
+                                  onChange={(event) => {
+                                    this.props.updateShelf(
+                                      book,
+                                      event.target.value
+                                    )
                                   }}
-                                />
-                                <div className="book-shelf-changer">
-                                  <select onChange={(event)=>{this.props.updateShelf(book,event.target.value);}} value={book.shelf}>
-                                    <option value="move" disabled>
-                                      Move to...
-                                    </option>
-                                    <option value="currentlyReading">
-                                      Currently Reading
-                                    </option>
-                                    <option value="wantToRead">
-                                      Want to Read
-                                    </option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
+                                  value={book.shelf}
+                                >
+                                  <option value="move" disabled>
+                                    Move to...
+                                  </option>
+                                  <option value="currentlyReading">
+                                    Currently Reading
+                                  </option>
+                                  <option value="wantToRead">
+                                    Want to Read
+                                  </option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
                               </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors}</div>
                             </div>
-                          </li>
-                        )
-                      })}
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                          </div>
+                        </li>
+                      )
+                    })}
                 </ol>
               </div>
             </div>
@@ -70,91 +76,106 @@ class BookPage extends React.Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {this.props.books
-                      .filter((book) => book.shelf === "wantToRead")
-                      .map((book, index) => {
-                        return (
-                          <li key={index}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div
-                                  className="book-cover"
-                                  style={{
-                                    width: 128,
-                                    height: 193,
-                                    backgroundImage:
-                                      'url("' + book.imageLinks.thumbnail + '")'
+                  {this.props.books
+                    .filter((book) => book.shelf === "wantToRead")
+                    .map((book, index) => {
+                      return (
+                        <li key={index}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div
+                                className="book-cover"
+                                style={{
+                                  width: 128,
+                                  height: 193,
+                                  backgroundImage:
+                                    'url("' + book.imageLinks.thumbnail + '")'
+                                }}
+                              />
+                              <div className="book-shelf-changer">
+                                <select
+                                  onChange={(event) => {
+                                    this.props.updateShelf(
+                                      book,
+                                      event.target.value
+                                    )
                                   }}
-                                />
-                                <div className="book-shelf-changer">
-                                  <select onChange={(event)=>{this.props.updateShelf(book,event.target.value);}}value={book.shelf}>
-                                    <option value="move" disabled>
-                                      Move to...
-                                    </option>
-                                    <option value="currentlyReading">
-                                      Currently Reading
-                                    </option>
-                                    <option value="wantToRead">
-                                      Want to Read
-                                    </option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
+                                  value={book.shelf}
+                                >
+                                  <option value="move" disabled>
+                                    Move to...
+                                  </option>
+                                  <option value="currentlyReading">
+                                    Currently Reading
+                                  </option>
+                                  <option value="wantToRead">
+                                    Want to Read
+                                  </option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
                               </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors}</div>
                             </div>
-                          </li>
-                        )
-                      })}
-                  </ol> 
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                          </div>
+                        </li>
+                      )
+                    })}
+                </ol>
               </div>
             </div>
             <div className="bookshelf">
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  
-                    {this.props.books
-                      .filter((book) => book.shelf === "read")
-                      .map((book, index) => {
-                        return (
-                          <li key={index}>
-                            <div className="book">
-                              <div className="book-top">
-                                <div
-                                  className="book-cover"
-                                  style={{
-                                    width: 128,
-                                    height: 193,
-                                    backgroundImage:
-                                      'url("' + book.imageLinks.thumbnail + '")'
+                  {this.props.books
+                    .filter((book) => book.shelf === "read")
+                    .map((book, index) => {
+                      return (
+                        <li key={index}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div
+                                className="book-cover"
+                                style={{
+                                  width: 128,
+                                  height: 193,
+                                  backgroundImage:
+                                    'url("' + book.imageLinks.thumbnail + '")'
+                                }}
+                              />
+                              <div className="book-shelf-changer">
+                                <select
+                                  onChange={(event) => {
+                                    this.props.updateShelf(
+                                      book,
+                                      event.target.value
+                                    )
                                   }}
-                                />
-                                <div className="book-shelf-changer">
-                                  <select onChange={(event)=>{this.props.updateShelf(book,event.target.value);}}value={book.shelf}>
-                                    <option value="move" disabled>
-                                      Move to...
-                                    </option>
-                                    <option value="currentlyReading">
-                                      Currently Reading
-                                    </option>
-                                    <option value="wantToRead">
-                                      Want to Read
-                                    </option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                                </div>
+                                  value={book.shelf}
+                                >
+                                  <option value="move" disabled>
+                                    Move to...
+                                  </option>
+                                  <option value="currentlyReading">
+                                    Currently Reading
+                                  </option>
+                                  <option value="wantToRead">
+                                    Want to Read
+                                  </option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
                               </div>
-                              <div className="book-title">{book.title}</div>
-                              <div className="book-authors">{book.authors}</div>
                             </div>
-                          </li>
-                        )
-                      })}
-                  </ol>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                          </div>
+                        </li>
+                      )
+                    })}
+                </ol>
               </div>
             </div>
           </div>
