8b9c514125d9166cd1ca61887dd00c084fd1b271
JoeKrieger02 <krieger.joachim@hotmail.be>
2018-07-25 17:32:43 +0200

    Cleaner

--- src/App.js
+++ src/App.js
@@ -1,70 +1,61 @@
-import React  from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-//import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-import BookPage from './BookPage'
-import SearchPage from './SearchPage'
-import { BrowserRouter } from 'react-router-dom';
-
+import React from "react"
+import * as BooksAPI from "./BooksAPI"
+import "./App.css"
+import { Route } from "react-router-dom"
+import BookPage from "./BookPage"
+import SearchPage from "./SearchPage"
+import { BrowserRouter } from "react-router-dom"
 
 class App extends React.Component {
-  state= {
-    books: [],
-    queriedBooks: []
-   
+  state = {
+    books: []
   }
 
-headers = {
-  'Authorization': "sai",   
-  'Content-Type': 'application/json' 
-}
+  headers = {
+    Authorization: "Joach"
+  }
 
-componentDidMount(){
-	BooksAPI.getAll().then((books) => {
-		this.setState({ books })
-		console.log(JSON.stringify(books))
-	})
-}
-/*
-findBook = (query) ={
- BooksAPI.search(query).then((result) => {
- this.setState({queriedBooks: []
-               })}
-*/
-updateShelf=(book, shelf)=> {
-	BooksAPI.update(book,shelf).then(()=>{
-      //this.getBooks()
-      BooksAPI.getAll().then((books)=>{
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
-    })
-
   }
 
-
+  updateShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books })
+      })
+    })
+  }
 
   render() {
     return (
-    <BrowserRouter>
-      <div className="app">
-      	<Route exact path="/" render={()=> (
-    		<BookPage 
-    			updateShelf={this.updateShelf}
-				books={this.state.books}
-			/>
-		)}/>
-		<Route path="/Search" render={()=> (
-      		<SearchPage
-             	books={this.state.books}
-				updateShelf={this.updateShelf}
-			/>
-		)}/>
-	</div> 
-</BrowserRouter>
+      <BrowserRouter>
+        <div className="app">
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <BookPage
+                updateShelf={this.updateShelf}
+                books={this.state.books}
+              />
+            )}
+          />
+          <Route
+            path="/Search"
+            render={() => (
+              <SearchPage
+                books={this.state.books}
+                updateShelf={this.updateShelf}
+              />
+            )}
+          />
+        </div>
+      </BrowserRouter>
     )
   }
-
 }
 
 export default App
